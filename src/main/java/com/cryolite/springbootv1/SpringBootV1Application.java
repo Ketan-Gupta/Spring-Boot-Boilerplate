37192f5a9cf53b7e5e3e02ba8746de6763b30f4f
Ketan Gupta <Ketan.Gupta@cdk.com>
2020-05-10 17:05:22 +0530

    Initializer- Starter Project

---
+++ src/main/java/com/cryolite/springbootv1/SpringBootV1Application.java
@@ -0,0 +1,14 @@
+package com.cryolite.springbootv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootV1Application.class, args);
+		System.out.println("Hello Spring Boot");
+	}
+
+}
---
+++ src/test/java/com/cryolite/springbootv1/SpringBootV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryolite.springbootv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
